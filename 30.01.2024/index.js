bdfd502fa06ee0102357ed59231d3d5c31aa14b1
sandro <modusi@gmail.com>
2024-01-30 19:46:11 +0400

    jc

---
+++ 30.01.2024/index.js
@@ -0,0 +1,64 @@
+// let a=15; //number
+// let b='16'; //string
+// let c='beqa' //string
+// let d=true  //Boolean
+// let e=false //Boolean
+// let f=undefined
+// let g=null
+// const array=['giorgi','sandro','dachi'] //array
+
+// let num1=3;
+// let num2=7;
+// let str='hello'
+
+// const res=str+num1+num2
+
+// console.log(res)
+
+// let firstName='sandro';
+// let lastName='xosse';
+// let fullName=firstName+lastName
+// console.log(fullName)
+
+// firstName='irakli';
+// lastName='kaka';
+// fullName=firstName+lastName
+// console.log(fullName);
+
+// function sum(a,b){
+//     return a/b
+// }
+
+// let num1=21;
+// let num2=7;
+// let num3=sum(num1,num2);
+
+// console.log(num3)
+
+// function gayofa(a,b){
+//     if(b===0){
+//         'second parameter not equal zero'
+//     }
+//     else{
+//         return a/b
+//     }
+// }
+
+// let num5=gayofa(10,2)
+// console.log(num5)
+
+function test(c) {
+    if (c < 15) {
+        return 'ჩაიცვი პალტო'
+    }
+    else if (c >= 15 && c <= 25) {
+        return 'ჩაიცვი ჯემპრი'
+    }
+    else {
+        return 'ჩაიცვი მაისური'
+    }
+}
+
+console.log(test(26))
+console.log(test(22))
+console.log(test(8))
