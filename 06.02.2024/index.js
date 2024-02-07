a489678ff17dcef2dc801d54ca335bcb34cbae63
sandro <modusi@gmail.com>
2024-02-07 18:09:23 +0400

    bs

---
+++ 06.02.2024/index.js
@@ -0,0 +1,165 @@
+// const cars=['saab','bmw','volvo']
+// // console.log(cars[1])
+
+// addToCars('opel')
+// addToCars('ferrari')
+// cars.push('dsd')
+// cars.pop()
+// cars.shift()
+// cars.unshift('ddd')
+
+
+// console.log(cars)
+
+
+
+// function addToCars(a){
+//     cars.push(a)
+// }
+
+
+
+// const teams=['Milan','Barcelona','Real','Bayern']
+// console.log(teams)
+// teams.push('Arsenal')
+// console.log(teams)
+// teams.push('ManSity')
+// console.log(teams)
+// teams.push('Chelsea')
+// console.log(teams)
+// teams.push('Dortmund')
+// console.log(teams)
+// teams.push('Leverkuzen')
+// console.log(teams)
+
+// teams.unshift('Juventus')
+// console.log(teams)
+// teams.unshift('Valencia')
+// console.log(teams)
+// teams.unshift('Napoli')
+// console.log(teams)
+// teams.unshift('Betis')
+// console.log(teams)
+// teams.unshift('Metalurgi')
+// console.log(teams)
+
+// teams.pop()
+// console.log(teams)
+// teams.pop()
+// console.log(teams)
+// teams.pop()
+
+// console.log(teams)
+// teams.shift()
+// console.log(teams)
+// teams.shift()
+// console.log(teams)
+// teams.shift()
+
+// console.log(teams)
+
+
+
+// console.log(arr.length)
+
+// console.log(arr)
+
+// arr.pop()
+// arr.shift()
+// arr.unshift('member-4')
+// arr.push('member-1')
+// console.log(arr)
+
+// const first=arr[0];
+// console.log(first)
+// const last=arr[3];
+// console.log(last)
+// arr[0]=last
+// arr[3]=first;
+// console.log(arr)
+
+// const first=arr[0]
+// const last=arr[arr.lenght-1]
+// arr[0]=last
+// arr[arr.lenght-1]=first;
+
+// console.log(arr)
+
+
+
+
+// const lastIndex=getlastIndex(arr) 
+
+// const arr=['member-1','member-2','member-3','member-4']
+
+// function array(a){
+//     if (a=arr ) {
+//         return 'this is array'
+//     }
+
+//     else { 
+//          'This is not array'
+//     }
+// }
+
+// function getlastIndex(a){
+//     if(Array.isArray(a)){
+//         const lastIndex=arr.lenght-1
+//         return lastIndex
+//     }
+//     else {
+//         "this is not array"
+//     }
+// }
+
+
+// console.log(getlastIndex(arr))
+// console.log(array())
+
+// console.log(arr.join('//'))
+
+// console.log(arr.concat(arr2))
+
+// const arr=['member-1','member-2','member-3','member-4']
+// const arr2=['1','2','3']
+
+// console.log([...arr,...arr2])
+
+
+// const italy=['milan','juventus','napoli']
+// const england=['mansity','liverpool','arsenal']
+// const espania=['real','barcelona','atletico']
+
+// const fullArray=[...italy,...england,...espania]
+
+
+// // console.log(fullArray)
+// console.log(fullArray.join('-'))
+
+
+// const fruits= ['Banana','Orange','Apple','Mango','Lemon']
+
+
+// function test(arr,start,quant){
+//     const coppyArray=[...arr];
+//     const result=coppyArray.splice(start,quant)
+//     return result
+// }
+
+// console.log(test(fruits,2,2))
+// console.log(fruits)
+
+const fruits= ['Banana','Orange','Apple','Mango','Lemon']
+
+// console.log(fruits)
+// console.log(fruits.slice(2,4))
+// console.log(fruits) 
+
+
+// console.log(fruits)
+// console.log(fruits.splice(3,2))
+// console.log(fruits)
+
+console.log(fruits)
+console.log(fruits.splice(2,4))
+console.log(fruits)
---
+++ 07.02.2024/index.js
@@ -0,0 +1,53 @@
+// const arr=[1,2,3,4,5,6,7,8,9]
+
+// for(let i=0; i<arr.length; i++){
+//     // arr[i]+=10;
+//     arr[i]=arr[i]+10
+// }
+
+// console.log(arr)
+
+// const arr=[1,2,3,4,5,6,7,8,9]
+// const arr2=[]
+// const arr3=[]
+
+// for(let i=0; i<arr.length; i++){
+//     arr[i]=arr[i]*10
+// }
+
+// console.log(arr)
+
+
+// for(let i=0; i; i++){
+//     if(arr[i]%2>0){
+//         arr2.push
+//     }
+// }
+
+// for(let i=0; i<arr.lenght; i++){
+//     if(arr[i]%2>0){
+        
+//     }
+// }
+
+// const arr=[[3,4,5],[6,7,8,4],[9,7,8,5,6]]
+// console.log(arr)
+
+// for (let i=0; i<arr.length; i++){
+//     // console.log(arr[i])
+//     for (let g=0; g<arr[i].lenght; g++) {
+//         arr[i][g]+=10
+//     }
+// }
+
+splice
+
+const arr=[1,2,3,4,5,6,7,8,9,]
+
+function evenNumber(a){
+    if (arr)
+}
+
+for (let i=0; i<arr.length; i++){
+    
+}
