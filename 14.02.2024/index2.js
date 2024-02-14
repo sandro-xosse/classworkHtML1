6192e9d7dd5f4d428c671b574a70f9e8f609efab
sandro <modusi@gmail.com>
2024-02-14 17:44:18 +0400

    bs and js

---
+++ 14.02.2024/index2.js
@@ -0,0 +1,14 @@
+// const arr=[11,24,55,74,34]
+
+// arr.forEach((i) => {
+//     console.log(i);
+// })
+
+// const filtered = arr(filtered)
+
+const arr=['banana','avokado','msxali']
+
+arr.forEach((i)=>{
+    console.log(i)
+}
+)
