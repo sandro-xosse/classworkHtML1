f15af4d1a3b6b2f5e2c769f40e0cfb68f860af69
sandro <modusi@gmail.com>
2024-02-24 09:41:36 +0000

    js

--- 20.02.2024/index.js
+++ 20.02.2024/index.js
@@ -94,34 +94,111 @@
 
 
 
-const products = [
-    { name: "Product 1", price: 20, category: "Electronics" },
-    { name: "Product 2", price: 30, category: "Clothes" },
-    { name: "Product 3", price: 40, category: "Electronics" },
-    { name: "Product 4", price: 50, category: "Clothes" },
-    { name: "Product 5", price: 70, category: "Clothes" },
-    { name: "Product 6", price: 70, category: "Electronics" },
-    { name: "Product 7", price: 80, category: "Clothes" },
-    { name: "Product 8", price: 90, category: "Electronics" },
-  ];
+// const products = [
+//   { name: "Product 1", price: 20, category: "Electronics" },
+//   { name: "Product 2", price: 30, category: "Clothes" },
+//   { name: "Product 3", price: 40, category: "Electronics" },
+//   { name: "Product 4", price: 50, category: "Clothes" },
+//   { name: "Product 5", price: 70, category: "Clothes" },
+//   { name: "Product 6", price: 70, category: "Electronics" },
+//   { name: "Product 7", price: 80, category: "Clothes" },
+//   { name: "Product 8", price: 90, category: "Electronics" },
+// ];
 
 
-  function getData(productArray){
-    const res=productArray.reduce((acum,item)=>{
-        let key=item.category;
-        if(acum[key]){
-            acum[key].push(item.price)
-        }
-        else{
-            acum[key]=[item.price]
-        }
-    },{})
+//   function getData(productArray){
+//     const res=productArray.reduce((acum,item)=>{
+//         let key=item.category;
+//         if(acum[key]){
+//             acum[key].push(item.price)
+//         }
+//         else{
+//             acum[key]=[item.price]
+//         }
+//     },{})
 
-    const res2=[];
-    for(let item in res){
-        let averagePrice=res[key].reduce((a,b)=>a+b)/item.length
-    }
+//     const res2=[];
+//     for(let item in res){
+//         let averagePrice=res[key].reduce((a,b)=>a+b)/item.length
+//     }
 
 
-  }
+//   }
 
+
+
+
+// const res= products.reduce((acum,item)=>{
+//     let key=item.category;
+//     if(acum[key]){
+//         acum[key].push(item.price)
+//         return acum
+//     }
+//     else{
+//         acum[key]=[item.price]
+//         return acum
+//     }
+// },{})
+
+
+// const res=products.reduce((acum,item)=>{
+//   let key=item.category;
+//   if(acum[key]){
+//       acum[key].push(item.price)
+//   }
+//   else{
+//       acum[key]=[item.price]
+//       return acum
+//   }
+// },{})
+
+
+
+// console.log(res)
+
+
+
+
+
+
+// home
+
+const myArray=[1,2,3,4,5,6,7,8,9,21,2,5,7,27,45]
+
+// 1)itration
+// 2)Array.prototype.LastindexOf
+// 3)Array.prototype.indexof
+
+
+
+
+
+
+
+
+
+
+
+
+//   const productsByCategory = products.reduce((acc, product) => {
+//     const category = product.category;
+//     if (!acc[category]) {
+//       acc[category] = [];
+//     }
+//     acc[category].push(product);
+//     return acc;
+//   }, {});
+
+//   console.log(productsByCategory)
+
+//   const avgPriceByCategory = Object.keys(productsByCategory).map(category => {
+//     const sum = productsByCategory[category].reduce((acc, product) => acc + product.price, 0);
+//     return { category: category, average: sum / productsByCategory[category].length };
+//   });
+
+//   console.log(avgPriceByCategory)
+
+//   const highPricedCategories = avgPriceByCategory.filter(category => category.average > 50);
+
+//   console.log(highPricedCategories)
+  
