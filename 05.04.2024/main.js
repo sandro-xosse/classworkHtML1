1c0f90d48b87e5bbd314a8ab3e2d79bedf0f259d
sandro <modusi@gmail.com>
2024-04-10 19:35:31 +0400

    js exercise

--- 05.04.2024/main.js
+++ 05.04.2024/main.js
@@ -1,7 +1,6 @@
 const gridElements=Array.from(document.querySelectorAll('.grid-item'))
 const Winning_board=document.querySelector('.Winning_board');
-const X_playerIcon=document.querySelector('.X_icon')
-const O_playerIcon=document.querySelector('.O_icon')
+
 
 // const X_player=document.querySelector('./tic-tac-design/tic-tac-design/image/Xdark')
 // console.log(X_player)
@@ -19,7 +18,6 @@ function handClick(array){
         element.addEventListener('click',(e)=>{
             changePlayer(element);
             winningLines(gridElements);
-            PlayerItem(gridElements)
         })
         
     });
@@ -28,57 +26,54 @@ function handClick(array){
 handClick(gridElements);
 
 
+
+
+// changeClass(gridElements)
+
 function changePlayer(el){
     if(!isEmpty(el)) return;    
-    if(p) el.innerHTML='X'
-    else el.innerHTML='0'
+    if(p) el.innerHTML=`<img src="./tik-tak-desing/tik-tak-desing/images/Xdark.svg" id="x"  alt="">`
+    else el.innerHTML=`<img src="./tik-tak-desing/tik-tak-desing/images/Odark.svg" id="o"  alt="">`
     p=!p
 }
 
 function isEmpty(element){
-      if (!element.innerText)  return true;
+      if (Array.from(element.children).length===0)  return true;
       return false;
 }
 
 
 function winningLines(arr){ 
-    if (arr[0].innerText===arr[1].innerText && arr[1].innerText===arr[2].innerText&&!!arr[0].innerText)
-    console.log('winner is aaa')
+    if (arr[0].children[0]?.id===arr[1].children[0]?.id && arr[1].children[0]?.id===arr[2].children[0]?.id && !!arr[0].children[0]?.id)
+     return console.log('winner is aaa')
 
-    if (arr[3].innerText===arr[4].innerText && arr[4].innerText===arr[5].innerText &&!!arr[3].innerText) 
+    if (arr[3].children[0]?.id===arr[4].children[0]?.id && arr[4].children[0]?.id===arr[5].children[0]?.id && !!arr[3].children[0]?.id) 
     return console.log('winner is bbb')
 
-    if (arr[6].innerText===arr[7].innerText && arr[7].innerText===arr[8].innerText &&!!arr[6].innerText)
+    if (arr[6].children[0]?.id===arr[7].children[0]?.id && arr[7].children[0]?.id===arr[8].children[0]?.id && !!arr[6].children[0]?.id)
     return console.log('winner is ccc')
 
-    if (arr[0].innerText===arr[3].innerText && arr[3].innerText===arr[6].innerText &&!!arr[0].innerText)
+    if (arr[0].children[0]?.id===arr[3].children[0]?.id && arr[3].children[0]?.id===arr[6].children[0]?.id && !!arr[0].children[0]?.id)
     return console.log('winner is ddd')
 
-    if (arr[1].innerText===arr[4].innerText && arr[4].innerText===arr[7].innerText &&!!arr[1].innerText)
+    if (arr[1].children[0]?.id===arr[4].children[0]?.id && arr[4].children[0]?.id===arr[7].children[0]?.id && !!arr[1].children[0]?.id)
     return console.log('winner is eee')
 
-    if (arr[2].innerText===arr[5].innerText && arr[5].innerText===arr[8].innerText &&!!arr[2].innerText)
+    if (arr[2].children[0]?.id===arr[5].children[0]?.id && arr[5].children[0]?.id===arr[8].children[0]?.id && !!arr[2].children[0]?.id)
     return console.log('winner is fff')
 
-    if (arr[0].innerText===arr[4].innerText && arr[4].innerText===arr[8].innerText &&!!arr[0].innerText)
+    if (arr[0].children[0]?.id===arr[4].children[0]?.id && arr[4].children[0]?.id===arr[8].children[0]?.id && !!arr[0].children[0]?.id)
     return console.log('winner is ggg')
 
-    if (arr[2].innerText===arr[4].innerText && arr[4].innerText===arr[6].innerText &&!!arr[2].innerText)
+    if (arr[2].children[0]?.id===arr[4].children[0]?.id && arr[4].children[0]?.id===arr[6].children[0]?.id && !!arr[2].children[0]?.id)
     return console.log('winner is hhh')
 
-
-
 }
 
 
-function PlayerItem(){
-    const Player=gridElements
-    if(Player.innerHTML==='X'){
-        X_playerIcon.classList.add("X_active")
-    }
-    console.log(gridElements)
-}
-
-
-
-PlayerItem(gridElements)
+// function PlayerItem(){
+//     const Player=gridElements
+//     if(Player.innerHTML==='X'){
+//         X_playerIcon.classList.add("X_active")
+//     }
+// }
---
+++ 10.04.2024/index.js
@@ -0,0 +1,83 @@
+
+// const numbers=[-5, -2, -6, 0, -1]
+
+// const max = Math.max(...numbers)
+// console.log(max)
+
+// for(let i=0; i < numbers.length; i++){
+
+// }
+
+// numbers.sort((a,b)=>b-a)
+// console.log(numbers[0])
+
+// ----------------------------------------------------------------------
+
+
+// for(let i=0; i < 16; i++){
+//     if (i % 2 > 0){
+//         console.log(`${i} is odd`)
+//     }
+//     else {
+//         console.log(`${i} is even`)
+//     }
+// }
+
+
+// -------------------------------------------------------------------
+
+// let arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+// arr1.sort((a,b)=>a-b)
+// console.log(arr1)
+
+
+// 4)Write a JavaScript program to find the most frequent item in an array.
+
+// let arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+// const ={}
+// let=
+// let=
+
+// 5)Write a JavaScript program that prints the elements of the following array.
+// Note : Use nested for loops.
+
+// let a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// a.forEach (myfunction)
+
+// function myfunction(arr){
+//     {
+//         console.log(arr[0]);
+//         console.log(arr[1]);
+//         console.log(arr[2]);
+//         console.log(arr[3]);
+//     }
+// }
+
+// 6) Write a JavaScript function to remove a specific element from an array.
+
+// Test data :
+// console.log(remove_array_element([2, 5, 9, 6], 5));
+// [2, 9, 6]
+
+const arr=[2, 5, 9, 6]
+function remove_array_element(arr,num){
+    for(let i=0; i<arr.length; i++){
+        if(
+            num=
+        )
+    }   
+    }
+
+console.log(remove_array_element([2, 5, 9, 6], 5))
+// 8)შექმენით ღილაკი რომლის ქლიქზე დარეფრეშდება ფეიჯი
+
+// const btn=document.querySelector('.btn')
+
+
+// btn.addEventListener('click',()=>{window.location.reload()
+// })
+
+
